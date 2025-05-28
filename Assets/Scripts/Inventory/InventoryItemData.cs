ee1630cd4ade9b7b3b3905257820aa89b93b07a8
withdongyeong <withdongyeong@gmail.com>
2025-05-28 16:10:35 +0900

    empty SO

---
+++ Assets/Scripts/Inventory/InventoryItemData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+/// <summary>
+/// 인벤토리 아이템 데이터 클래스
+/// </summary>
+///
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class InventoryItemData : ScriptableObject
+{
+}
