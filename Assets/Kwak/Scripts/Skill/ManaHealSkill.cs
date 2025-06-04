7c81aa3ea53ca5f354c2437d7ba4b344b02c2604
scopedogtc <rhdrks1440@gmail.com>
2025-06-04 16:22:38 +0900

    배치된 오브젝트 움직이는것 구현

---
+++ Assets/Kwak/Scripts/Skill/ManaHealSkill.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using static BaseTile;
+
+public class ManaHealSkill : SkillBase
+{
+    [SerializeField] protected int _healAmount = 25;
+    [SerializeField] protected GameObject _healEffectPrefab;
+
+    public int HealAmount { get => _healAmount; set => _healAmount = value; }
+
+    private PlayerMana _playerMana;
+
+    private void Start()
+    {
+        _playerMana = FindAnyObjectByType<PlayerMana>();
+    }
+
+
+    /// <summary>
+    /// 플레이어 마나 회복 처리
+    /// </summary>
+    private void HealPlayer()
+    {
+        // 플레이어 체력 회복
+        _playerMana.ModifyCurrentMana(HealAmount);
+
+        // 회복 이펙트 생성
+        CreateHealEffect();
+    }
+
+    /// <summary>
+    /// 회복 이펙트 생성
+    /// </summary>
+    private void CreateHealEffect()
+    {
+        if (_healEffectPrefab != null && _playerMana != null)
+        {
+            // 플레이어 위치에 회복 이펙트 생성
+            GameObject effectObj = Instantiate(
+                _healEffectPrefab,
+                _playerMana.transform.position,
+                Quaternion.identity
+            );
+
+            // 일정 시간 후 이펙트 제거
+            Destroy(effectObj, 0.1f);
+        }
+    }
+
+    protected override void Activate(GameObject user)
+    {
+        HealPlayer();
+    }
+}
