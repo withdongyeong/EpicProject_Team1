019b6d1305a32887e716e8e631a655f62bc3dd58
gwnoo <gwpak21@gmail.com>
2025-06-13 10:28:16 +0900

    [fix] 정보창 위치 변경
    정보창 위치가 매번 바뀌니 화면 밖으로 삐져나가는 현상이 발생하여 정보창 위치를 고정시켰습니다.

--- Assets/@Cosmos/Scripts/UI/InfoPanel.cs
+++ Assets/@Cosmos/Scripts/UI/InfoPanel.cs
@@ -106,39 +106,44 @@ public class InfoPanel : MonoBehaviour
 
     private void UpdatePosition(Vector3 position, bool isUIElement)
     {
-        Vector2 mousePos;
+        //Vector2 mousePos;
 
+        //if (isUIElement)
+        //{
+        //    // UI 요소: 스크린 좌표를 캔버스 좌표로 변환
+        //    RectTransformUtility.ScreenPointToLocalPointInRectangle(
+        //        canvas.transform as RectTransform,
+        //        position,
+        //        canvas.worldCamera,
+        //        out mousePos
+        //    );
+        //}
+        //else
+        //{
+        //    // 3D/2D 오브젝트: 월드 좌표를 캔버스 좌표로 변환
+        //    Vector2 screenPos = mainCamera.WorldToScreenPoint(position);
+        //    RectTransformUtility.ScreenPointToLocalPointInRectangle(
+        //        canvas.transform as RectTransform,
+        //        screenPos,
+        //        canvas.worldCamera,
+        //        out mousePos
+        //    );
+        //}
+
+        //// 정보 UI 위치 설정 (오프셋 적용)
+        ////Vector2 offset = new Vector2(20f, 50f);
+        ////Vector2 newPos = mousePos;
+
+        ////// 캔버스 경계 내로 제한
+        ////Rect canvasRect = (canvas.transform as RectTransform).rect;
+        ////newPos.x = Mathf.Clamp(newPos.x, canvasRect.xMin, canvasRect.xMax - rectTransform.rect.width);
+        ////newPos.y = Mathf.Clamp(newPos.y, canvasRect.yMin + rectTransform.rect.height, canvasRect.yMax);
+
+        Vector2 newPos = new Vector2(110, 30);
         if (isUIElement)
         {
-            // UI 요소: 스크린 좌표를 캔버스 좌표로 변환
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                canvas.transform as RectTransform,
-                position,
-                canvas.worldCamera,
-                out mousePos
-            );
+            newPos = new Vector2(-35, 30);
         }
-        else
-        {
-            // 3D/2D 오브젝트: 월드 좌표를 캔버스 좌표로 변환
-            Vector2 screenPos = mainCamera.WorldToScreenPoint(position);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                canvas.transform as RectTransform,
-                screenPos,
-                canvas.worldCamera,
-                out mousePos
-            );
-        }
-
-        // 정보 UI 위치 설정 (오프셋 적용)
-        //Vector2 offset = new Vector2(20f, 50f);
-        Vector2 newPos = mousePos;
-
-        // 캔버스 경계 내로 제한
-        Rect canvasRect = (canvas.transform as RectTransform).rect;
-        newPos.x = Mathf.Clamp(newPos.x, canvasRect.xMin, canvasRect.xMax - rectTransform.rect.width);
-        newPos.y = Mathf.Clamp(newPos.y, canvasRect.yMin + rectTransform.rect.height, canvasRect.yMax);
-
         rectTransform.anchoredPosition = newPos;
     }
 }
