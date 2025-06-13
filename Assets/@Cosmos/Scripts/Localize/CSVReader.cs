d9e158a239a0ea20c583833dfa39636ebe02a1ba
6767cc <116047063+6767cc@users.noreply.github.com>
2025-06-13 17:06:56 +0900

    [feat]로컬라이징 구현중

---
+++ Assets/@Cosmos/Scripts/Localize/CSVReader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+public class CSVReader : Singleton<CSVReader>
+{
+    public List<string[]> NotObjectText_List = new List<string[]>();
+
+    protected override void Awake()
+    {
+        base.Awake();  // 싱글톤 체크
+
+        string NotObjectText_CSV = Path.Combine(Application.streamingAssetsPath, "오브젝트외텍스트.csv");
+
+        FileToString(NotObjectText_CSV, NotObjectText_List);
+    }
+
+    private void FileToString(string filePath, List<string[]> StoryText)
+    {
+        if (File.Exists(filePath))
+        {
+            // UTF-8 인코딩으로 파일 읽기
+            string[] lines = File.ReadAllLines(filePath, Encoding.UTF8);
+
+            foreach (string line in lines)
+            {
+                string[] values = line.Split(',');
+                StoryText.Add(values);
+            }
+        }
+        else
+        {
+            Debug.LogError("CSV 파일을 찾을 수 없습니다!");
+        }
+    }
+}
