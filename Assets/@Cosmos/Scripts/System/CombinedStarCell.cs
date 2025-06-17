a15c3cd2ff9af2fd224e98982794e2e9cfcbd970
scopedogtc <rhdrks1440@gmail.com>
2025-06-17 16:03:39 +0900

    [feat] 타일 제작 (#84)
    StarCell들을 CombinedStarCell밑에 묶어서 관리할 수 있게 했습니다.

---
+++ Assets/@Cosmos/Scripts/System/CombinedStarCell.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CombinedStarCell : MonoBehaviour
+{
+    private StarBase starSkill;
+
+    private void Awake()
+    {
+        starSkill = GetComponent<StarBase>();
+        if (starSkill == null)
+        {
+            Debug.LogError("스타스킬이 없는데요 ???? ");
+        }
+    }
+
+    public StarBase GetStarSkill()
+    {
+        return starSkill;
+    }
+}
