0927788b4de24ff40efcacd63eb7f6f83423b021
scopedogtc <rhdrks1440@gmail.com>
2025-06-18 11:17:38 +0900

    [fix] 타일 제작 #84
    토템 타일이 인스펙터가 아닌 Resources에서 로드된 프리팹을 참고하도록 변경하였습니다. 이 과정에서 타일이 드래그 될때 이름이 dragObject가 되던 사항을 변경하였습니다.

--- Assets/@Cosmos/Scripts/Skill/Summon/TotemSummonSkill.cs
+++ Assets/@Cosmos/Scripts/Skill/Summon/TotemSummonSkill.cs
@@ -1,12 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TotemSummonSkill : SkillBase
 {
-    [SerializeField] private GameObject _totem;
+    private GameObject _totem;
 
     [SerializeField] int _totemPower;
 
     private TotemManager _totemManager;
+
+    protected override void Start()
+    {
+        base.Start();
+        //먼저 타일 오브젝트의 이름을 가져옵니다
+        string coreName = GetComponent<CombineCell>().GetTileObject().name;
+
+        //Totem이라는 문자를 찾고 그 앞부터 토템까지만 잘라옵니다
+        int index = coreName.IndexOf("Totem");
+        string result = index >= 0
+            ? coreName.Substring(0, index + "Totem".Length)
+            : coreName; // "here"가 없으면 전체 반환
+
+        //잘라온 결과와 동일한 이름을 가진 토템을 로드해옵니다.
+        string filePath = "Prefabs/Summons/Totem/" + result;
+        _totem = Resources.Load<GameObject>(filePath);
+    }
 
     protected override void Activate(GameObject user)
     {
