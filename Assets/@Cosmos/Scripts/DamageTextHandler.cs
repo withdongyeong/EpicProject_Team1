a2a80aa9a29f2ed04e6697bb5ccea2d15c635436
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-06-23 17:37:28 +0900

    [fix] 몹 머리 위로 데미지 표기 , 효과 추가

--- Assets/@Cosmos/Scripts/DamageTextHandler.cs
+++ Assets/@Cosmos/Scripts/DamageTextHandler.cs
@@ -7,7 +7,6 @@ public class DamageTextHandler : MonoBehaviour
     public bool isVer2 = false; // true면 UI로 표시, false면 월드 공간에 표시
     //Assign prefab in inspector.(임시)
     public DamageNumber numberPrefab;
-    public DamageNumber numberPrefabUI;
     public RectTransform rectParent;
     private BaseBoss target;
     private void Awake()
@@ -23,25 +22,16 @@ public class DamageTextHandler : MonoBehaviour
 
     public void SpawnDamageText(int damage)
     {
-        if (isVer2)
-        {
-            Transform targetPos = GameObject.Find("TestDamagePos").transform;
-            Vector2 screenPos = Camera.main.WorldToScreenPoint(targetPos.position);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectParent, screenPos, null,
-                out Vector2 localPoint);
-            DamageNumber damageNumber = numberPrefabUI.SpawnGUI(rectParent, localPoint, damage);
-            //damageNumber.followedTarget = target.transform;
-        }
-        else
-        {
-            Vector2 screenPos = Camera.main.WorldToScreenPoint(target.transform.position + Vector3.up * 3f);
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(rectParent, screenPos, null,
-                out Vector2 localPoint);
-            DamageNumber damageNumber = numberPrefab.SpawnGUI(rectParent, localPoint, damage);
-            damageNumber.followedTarget = target.transform;
-            // You can customize the damage number here if needed
-            // e.g., damageNumber.SetColor(Color.red);
-        }
+        
+        
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(target.transform.position + Vector3.up * 3f);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(rectParent, screenPos, null,
+            out Vector2 localPoint);
+        DamageNumber damageNumber = numberPrefab.SpawnGUI(rectParent, localPoint, damage);
+        damageNumber.followedTarget = target.transform;
+        // You can customize the damage number here if needed
+        // e.g., damageNumber.SetColor(Color.red);
+    
     }
 
 
