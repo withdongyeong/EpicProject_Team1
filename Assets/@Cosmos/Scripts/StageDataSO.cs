15f5a0a28f33918dccf299407c34d50e8d6c5864
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-06-23 11:47:00 +0900

    [REFAC] StageSelectManager 생성
    기존 StageSelecHandler를 Manager로 변경, 이제 스테이지는 StageDataSO로 동적 데이터 관리합니다.

---
+++ Assets/@Cosmos/Scripts/StageDataSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Stage", menuName = "NewStage/StageData")]
+public class StageDataSO : ScriptableObject
+{
+    public GameObject enemyPrefab;
+    public Sprite backgroundSprite;
+    public AudioClip bgmClip; 
+}
