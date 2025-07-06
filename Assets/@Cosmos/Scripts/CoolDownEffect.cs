472fc3bcab100ced7eb43b3ec2e647d9f07f908d
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-07-07 03:49:57 +0900

    [feat] 쿨타임 진짜 진짜 거의 99% 완성

--- Assets/@Cosmos/Scripts/CoolDownEffect.cs
+++ Assets/@Cosmos/Scripts/CoolDownEffect.cs
@@ -1,25 +1,51 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class CoolDownEffect : MonoBehaviour
 {
-    private int coolDownPoint = 0;
+    [SerializeField]
+    private float coolDownPoint = 0;
     private SpriteRenderer sr;
     public void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
-        EventBus.SubscribeSceneLoaded(SetPosition);
+        EventBus.SubscribeGameStart(SetPosition);
+        EventBus.SubscribeBossDeath(Init);
     }
 
-    
+    public void Init()
+    {
+        StopAllCoroutines();
+        sr.size = new Vector2(1, 0);
+    }
+    public void StartCoolDown(float coolDownTime)
+    {
+        sr.size = new Vector2(1, 0);
+        coolDownPoint = 0;
+        StartCoroutine(CoolDownCoroutine(coolDownTime));
+    }
+    private IEnumerator CoolDownCoroutine(float coolDownTime)
+    {
+        while (coolDownPoint < 1)
+        {
+            coolDownPoint += Time.deltaTime / coolDownTime;
+            //sr.color = new Color(1, 1, 1, 1 - coolDownPoint); // 알파값을 감소시켜 투명해짐
+            sr.size = new Vector2(1, 1 * coolDownPoint);
+            yield return null;
+        }
+        sr.size = new Vector2(1, 1);
+    }
     
     /// <summary>
     /// 씬이 로드될 때 호출되어 타일 회전에 따른 CoolDownEffect의 위치를 설정합니다.
     /// </summary>
-    public void SetPosition(Scene scene , LoadSceneMode mode)
+    public void SetPosition()
     {
         if (!SceneLoader.IsInStage()) return;
+        StopAllCoroutines();
+        sr.size = new Vector2(1, 0);
         if (transform.rotation.eulerAngles.z == 0)
         {
             transform.localPosition = new Vector3(0, -0.5f, 0);
@@ -45,6 +71,7 @@ public class CoolDownEffect : MonoBehaviour
 
     public void OnDestroy()
     {
-        EventBus.UnsubscribeSceneLoaded(SetPosition);
+        EventBus.UnsubscribeGameStart(SetPosition);
+        EventBus.UnsubscribeBossDeath(Init);
     }
 }
