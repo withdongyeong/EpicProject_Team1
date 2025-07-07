8311f61423f92df8a226eda9795c14fb71feaa07
scopedogtc <rhdrks1440@gmail.com>
2025-07-07 23:44:21 +0900

    [FIX] 타일 제작 #84
    여신상 처럼 밟아도 효과가 없는 타일들의 투명도를 낮추고 조명을 제거했습니다.

--- Assets/@Cosmos/Scripts/Skill/NonActivateSkill.cs
+++ Assets/@Cosmos/Scripts/Skill/NonActivateSkill.cs
@@ -1,11 +1,27 @@
-﻿using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class NonActivateSkill : SkillBase
 {
+    protected SpriteRenderer spriteRenderer;
+
+    protected Color originalColor;
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        spriteRenderer = transform.GetChild(0).GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
+    }
+
     protected override void Activate()
     {
 
     }
+    
+    //전투씬 돌입시 호출되는 함수입니다
     protected override void InitClearStarList(Scene scene, LoadSceneMode mode)
     {
         if (SceneLoader.IsInStage())
@@ -13,14 +29,27 @@ public class NonActivateSkill : SkillBase
             // 전투씬 시작시 인접 효과를 초기화합니다.
             ClearStarBuff();
             _coolTimeMaterial.SetFloat("_FillAmount", 0);
+            Color invisibleColor = originalColor;
+            invisibleColor.a = 0.15f;
+            spriteRenderer.color = invisibleColor;
+            foreach (Transform child in transform.GetChild(0))
+            {
+                child.gameObject.SetActive(false);
+            }
         }
     }
 
+    //상점씬 돌입시 호출되는 함수입니다.
     protected override void ResetCoolDown(Scene scene, LoadSceneMode mode)
     {
         if (SceneLoader.IsInBuilding())
         {
             _coolTimeMaterial.SetFloat("_FillAmount", 1);
+            spriteRenderer.color = originalColor;
+            foreach (Transform child in transform.GetChild(0))
+            {
+                child.gameObject.SetActive(true);
+            }
         }
     }
 
