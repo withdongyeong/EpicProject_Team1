b90a66ebaf3ffe36b3a0b815fce3e05b87eb83fa
scopedogtc <rhdrks1440@gmail.com>
2025-07-08 02:48:21 +0900

    [FIX] 튜토리얼 텍스트 수정 #339
    7번 가이드 텍스트를 수정하고 크기를 줄였습니다.

--- Assets/@Cosmos/Scripts/@GuideScene/FightQuest.cs
+++ Assets/@Cosmos/Scripts/@GuideScene/FightQuest.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 public class FightQuest : GuideQuest
@@ -14,7 +14,7 @@ public class FightQuest : GuideQuest
     
     public override void SetTexts()
     {
-        instructionText = "가이드 7\n ㄴ 적과 싸워 승리하세요\n (0/1)";
+        instructionText = "가이드 7\n ㄴ 타일을 밟아 적과 싸우세요\n (0/1)";
         titleText = "- 가이드 7 -";
         subTitleText = "승리하기";
         contentText = "전투 준비를 마치고\n우측하단 [전투시작]을 \n눌러 전투를 시작하세요\n\n그리고 별자리를 밟아 \n적을 공격해 승리하세요";
@@ -30,7 +30,7 @@ public class FightQuest : GuideQuest
     
     public override bool IsCompleted()
     {
-        instructionText = $"가이드 7\n ㄴ 적과 싸워 승리하세요\n ({count}/1)";
+        instructionText = $"가이드 7\n ㄴ 타일을 밟아 적과 싸우세요\n ({count}/1)";
         GuideHandler.instance.questText.text = instructionText;
         return count >= 1;
     }
