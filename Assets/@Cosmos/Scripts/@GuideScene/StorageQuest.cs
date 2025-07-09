96a20b4da4499933d79b57110965b01fa0686902
6767cc <116047063+6767cc@users.noreply.github.com>
2025-07-09 15:46:43 +0900

    [feat] 가이드 로컬라이징 제작
    폰트 때문에 몇몇 텍스트 깨지는 문제 있음
    수정 예정

--- Assets/@Cosmos/Scripts/@GuideScene/StorageQuest.cs
+++ Assets/@Cosmos/Scripts/@GuideScene/StorageQuest.cs
@@ -1,19 +1,30 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Localization;
 
 public class StorageQuest : GuideQuest
 {
     [SerializeField]
     public int count = 0;
-    
+
+    public LocalizedString instructionTextLocalized;
+    public LocalizedString titleTextLocalized;
+    public LocalizedString subTitleTextLocalized;
+    public LocalizedString contentTextLocalized;
+    public LocalizedString goalTextLocalized;
+
     public override void SetTexts()
     {
-        instructionText = "가이드 5\n ㄴ 별자리를 보관해보세요\n (0/4개)";
-        titleText = "- 가이드 5 -";
-        subTitleText = "별자리 보관하기";
-        contentText = "별자리는 영역의 양쪽 공간에\n드래그 하여 보관할 수 있습니다\n보관한 별자리는 \n전투에 사용할 수 없습니다\n";
-        goalText = "- 완료 조건 -\n" +
-                   "별자리 4개 보관하기 (0/4개)\n";
+        // 언어 설정에 따라 번역된 문자열 가져오기
+        instructionTextLocalized.StringChanged += (text) => {
+            instructionText = text.Replace("{0}", "0");
+        };
+
+        titleTextLocalized.StringChanged += (text) => titleText = text;
+        subTitleTextLocalized.StringChanged += (text) => subTitleText = text;
+        contentTextLocalized.StringChanged += (text) => contentText = text;
+        goalTextLocalized.StringChanged += (text) => goalText = text;
     }
+
     public override void OnStart()
     {
         
@@ -22,7 +33,9 @@ public class StorageQuest : GuideQuest
 
     public override bool IsCompleted()
     {
-        instructionText = $"가이드 5\n ㄴ 별자리를 보관해보세요\n ({count}/4개)";
+        instructionTextLocalized.StringChanged += (text) => {
+            instructionText = text.Replace("{0}", count.ToString());
+        };
         GuideHandler.instance.questText.text = instructionText;
         return count >= 4;
     }
