cc4fd8bb2e822029f8698782b48ba28f99dd9435
yhaaan <122730802+yhaaan@users.noreply.github.com>
2025-07-16 02:07:32 +0900

    [fix] 난이도로 로그로 보냄

--- Assets/@Cosmos/Scripts/System/Manager/AnalyticsManager.cs
+++ Assets/@Cosmos/Scripts/System/Manager/AnalyticsManager.cs
@@ -64,11 +64,13 @@ public class AnalyticsManager : Singleton<AnalyticsManager>
         string itemsJson = JsonConvert.SerializeObject(GridManager.Instance.GetPlacedTileCount()); // 아이템 사용 딕셔너리를 JSON 문자열로 변환합니다.
         int stageIndex =  StageSelectManager.Instance.StageNum;
         float stageClearTime = GameManager.Instance.LogHandler.GetStageClearTimer();
+        int difficultyLevel = GameManager.Instance.DifficultyLevel;
         
         
         // 2. 'stage_clear' 이벤트를 생성하고 파라미터를 담습니다.
         CustomEvent stageClearEvent = new CustomEvent("stage_clear")
         {
+            { "difficulty_level", difficultyLevel },
             { "stage_index", stageIndex },
             { "clear_time", stageClearTime },
             { "used_items",  itemsJson}
