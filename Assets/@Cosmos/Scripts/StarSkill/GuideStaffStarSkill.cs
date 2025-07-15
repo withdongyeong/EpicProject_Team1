dbc7620f8e2a9b3ab6d0761b41cc29c5cb2286d0
gwnoo <gwpak21@gmail.com>
2025-07-15 21:28:44 +0900

    [feat] 가이드 스태프 별 스킬

---
+++ Assets/@Cosmos/Scripts/StarSkill/GuideStaffStarSkill.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class GuideStaffStarSkill : EnhancementStarSkill
+{
+    private bool isQuestCompleted = false;
+    public void QuestClear()
+    {
+        if (!isQuestCompleted)
+        {
+            Debug.Log("[튜토리얼] 가이드 스태프 스타 스킬 퀘스트 완료");
+            isQuestCompleted = true;
+        }
+    }
+    public void QuestReset()
+        {
+        if (isQuestCompleted)
+        {
+            Debug.Log("[튜토리얼] 가이드 스태프 스타 스킬 퀘스트 실패");
+            isQuestCompleted = false;
+        }
+    }
+}
