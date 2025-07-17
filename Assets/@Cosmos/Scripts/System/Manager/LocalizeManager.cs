ecb9ad5a1c8eeb8d19d48474157afd251678edd4
6767cc <116047063+6767cc@users.noreply.github.com>
2025-07-17 16:39:47 +0900

    [feat]로컬라이즈 메니저 구현중

--- Assets/@Cosmos/Scripts/System/Manager/LocalizeManager.cs
+++ Assets/@Cosmos/Scripts/System/Manager/LocalizeManager.cs
@@ -7,6 +7,83 @@ public class LocalizeManager : Singleton<LocalizeManager>
         base.Awake();
     }
 
+
+
+    public string Local_Quset_Instruction_Text(string key)
+    {
+        string ReturnText = "";
+        LocalizedString QuestText = new LocalizedString("EpicProject_Table", "UI_Quest_Instruction_" + key);
+        QuestText.StringChanged += (value) =>
+        {
+            ReturnText = value;
+        };
+        return ReturnText;
+    }
+
+    public string Local_Quset_Title_Text(string key)
+    {
+        string ReturnText = "";
+        LocalizedString QuestText = new LocalizedString("EpicProject_Table", "UI_Quest_Title_" + key);
+        QuestText.StringChanged += (value) =>
+        {
+            ReturnText = value;
+        };
+        return ReturnText;
+    }
+
+    public string Local_Quset_SubTitle_Text(string key)
+    {
+        string ReturnText = "";
+        LocalizedString QuestText = new LocalizedString("EpicProject_Table", "UI_Quest_SubTitle_" + key);
+        QuestText.StringChanged += (value) =>
+        {
+            ReturnText = value;
+        };
+        return ReturnText;
+    }
+
+    public string Local_Quset_Content_Text(string key)
+    {
+        string ReturnText = "";
+        LocalizedString QuestText = new LocalizedString("EpicProject_Table", "UI_Quest_Content_" + key);
+        QuestText.StringChanged += (value) =>
+        {
+            ReturnText = value;
+        };
+        return ReturnText;
+    }
+
+    public string Local_Quset_Goal_Text(string key)
+    {
+        string ReturnText = "";
+        LocalizedString QuestText = new LocalizedString("EpicProject_Table", "UI_Goal_Instruction_" + key);
+        QuestText.StringChanged += (value) =>
+        {
+            ReturnText = value;
+        };
+        return ReturnText;
+    }
+
+
+    /// <summary>
+    /// 타일 해금 텍스트 표시
+    /// </summary>
+    /// <param name="StageNumber"></param>
+    /// <returns></returns>
+    public string Local_TileUnlockConditions(int StageNumber)
+    {
+        string ReturnText = "";
+
+        LocalizedString TileUnlockConditionsText = new LocalizedString("EpicProject_Table", "UI_Text_TileUnlockConditions");
+
+        TileUnlockConditionsText.StringChanged += (value) =>
+        {
+            ReturnText = value.Replace("{0}", StageNumber.ToString());
+        };
+
+        return ReturnText;
+    }
+
     public void TileCategoryTextChange(string tileCategory, System.Action<string> callback)
     {
         LocalizedString localizedString = new LocalizedString
